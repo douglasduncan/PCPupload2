6a72d4b17e05b0631eeebab4d85b61a67b1b47cb
douglas.duncan@btinternet.com <Itwouldbeashame25>
2019-02-03 20:51:15 +0000

    final commit 3/2/19

--- app/src/main/java/com/example/douglas/pcpupload/MainActivity.java
+++ app/src/main/java/com/example/douglas/pcpupload/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.douglas.pcpupload;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,23 +16,30 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
+
+
+
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+      //  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+      //  fab.setOnClickListener(new View.OnClickListener() {
+       //     @Override
+       //     public void onClick(View view) {
+       //         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //                .setAction("Action", null).show();
+       //     }
+      //  });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -41,6 +49,14 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        /////////////////////////////////////////////////////////////////////////my bit**************
+
+        calculaTor testing = new calculaTor();
+
+        int returnted = testing.dd(800);
+        Toast.makeText(this, ""+returnted, Toast.LENGTH_LONG).show();
+
     }
 
     @Override
@@ -78,6 +94,9 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+        //////////////////////////////////////////////////////////////////////////////intents for navigation
+        Intent intent = new Intent(this, addVehicle1.class);
+
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -90,8 +109,9 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
-
+        } else if (id == R.id.add_vehicle) {
+            Toast.makeText(this, "add vehicle", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
         } else if (id == R.id.nav_send) {
 
         }
--- app/src/main/java/com/example/douglas/pcpupload/calculaTor.java
+++ app/src/main/java/com/example/douglas/pcpupload/calculaTor.java
@@ -1,4 +1,21 @@
 package com.example.douglas.pcpupload;
 
+
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class calculaTor {
+
+    public int dd(int varry){
+
+        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        Log.i("message", "this is a message");
+        int new_varry = varry*4;
+        return new_varry;
+
+    }
+
+
 }
