f0d91dcfa048216d47c81c2fcea026a045e11d4a
douglas.duncan@btinternet.com <Itwouldbeashame25>
2019-02-04 21:15:09 +0000

    final commit 4/2/19

--- app/src/main/java/com/example/douglas/pcpupload/addVehicle1.java
+++ app/src/main/java/com/example/douglas/pcpupload/addVehicle1.java
@@ -1,7 +1,10 @@
 package com.example.douglas.pcpupload;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class addVehicle1 extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class addVehicle1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_vehicle1);
+
+        /////create shared preferences to be logged in.
+        SharedPreferences.Editor editor = getSharedPreferences("InitialPrefs", MODE_PRIVATE).edit();
+        Log.w("preferences", "setting up shared prefs");
+        String username = "testing username";
+        editor.putString("username", username);
+        Toast.makeText(this, "shared prefs created", Toast.LENGTH_SHORT).show();
+        editor.apply();
+
+
     }
 }
